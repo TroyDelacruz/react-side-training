f5ca4e716e156b1f8477674d58425575ce76e205
Troy Delacruz <troy8076@yahoo.com>
2023-01-12 16:14:15 -0500

    Reediting and working through the tutorial

--- tic-tac-toe/src/Square.js
+++ tic-tac-toe/src/Square.js
@@ -1,23 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 
 
 
-function Square() {
-    const [value, setValue] = useState(null);
-
-    function handleClick() {
-        setValue('X');
-    }
-
-    return (
-        <button
-          className="square"
-          onClick={handleClick}>
-
-            {value}
-          </button>
-    );
+function Square({ value, onSquareClick }) {
+    return <button className="square" onClick={onSquareClick}>{value}</button>
 }
-
 export default Square;
